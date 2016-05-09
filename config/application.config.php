6b06915eaee9ede703c653d661d0d85aff95a92b
James Stormes <jstormes@stormes.net>
2016-05-09 11:17:00 -0500

    Testing

--- config/application.config.php
+++ config/application.config.php
@@ -2,8 +2,7 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application',
-        'JStormes\SimpleSAML'
+        'Application'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
